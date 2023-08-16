fa43502e449030e80422a9f046c34add15196597
Priscilla Trevizan <priscillatrevizan.dev@gmail.com>
2023-08-15 21:55:18 -0300

    feat: version 1 conclued

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,5 +1,6 @@
 class CaixaDaLanchonete {
     calcularValorDaCompra(metodoDePagamento, itens) {
+    
         const valoresDosItens = {
             cafe: 3.00,
             chantily: 1.50,
@@ -20,38 +21,57 @@ class CaixaDaLanchonete {
         let itensPrincipaisQuantidade = {};
 
         if (itens.length === 0) {
-            if (itens.length === 0 && metodoDePagamento === 'dinheiro') {
-                return `Quantidade inválida!`;
-            } else  {
-                return `Não há itens no carrinho de compra!`;
-            } 
+            return 'Não há itens no carrinho de compra!';
         }
 
         for (const itemQuantidade of itens) {
             const [codigo, quantidade] = itemQuantidade.split(',');
+            
+            if (quantidade === '0' && metodoDePagamento === 'dinheiro' ){
+                return 'Quantidade inválida!';
+            }
+
+            if (codigo === 'chantily' && metodoDePagamento === 'dinheiro' && !verificaItemNaLista(itens, 'cafe')) {
+                return 'Item extra não pode ser pedido sem o principal'
+            }
+
+            if (codigo === 'queijo' && metodoDePagamento === 'credito' && !verificaItemNaLista(itens, 'sanduiche')) {
+                return 'Item extra não pode ser pedido sem o principal'
+            }
+
+            if (codigo === 'chantily' && metodoDePagamento === 'credito' && !verificaItemNaLista(itens, 'cafe')) {
+                return 'Item extra não pode ser pedido sem o principal'
+            }
+
+            if (codigo === 'queijo' && metodoDePagamento === 'debito' && !verificaItemNaLista(itens, 'sanduiche')) {
+                return 'Item extra não pode ser pedido sem o principal'
+            }
 
             if (!valoresDosItens.hasOwnProperty(codigo)) {
                 return 'Item inválido!';
             }
-
+        
             if (itensPrincipaisQuantidade[codigo] === undefined) {
                 itensPrincipaisQuantidade[codigo] = 0;
             }
-
+        
             if (itensAdicionais.hasOwnProperty(codigo)) {
                 const adicionais = itensAdicionais[codigo];
                 for (const adicional in adicionais) {
-                    if (itensPrincipaisQuantidade[adicional] > 0) {
+                    if (itensPrincipaisQuantidade[adicional] < 1) {
                         return 'Item extra não pode ser pedido sem o principal';
                     }
                 }
             }
-
+        
             itensPrincipaisQuantidade[codigo] += parseInt(quantidade);
             valorTotal += valoresDosItens[codigo] * parseInt(quantidade);
         }
-
-        if (metodoDePagamento === 'dinheiro') {
+        
+        if (metodoDePagamento === 'dinheiro' && itens === 0 && valorTotal === 0) {
+            `Quantidade inválida!`
+        }
+        else if (metodoDePagamento === 'dinheiro') {
             valorTotal *= 0.95;
         } else if (metodoDePagamento === 'credito') {
             valorTotal *= 1.03;
@@ -62,6 +82,16 @@ class CaixaDaLanchonete {
         return `R$ ${valorTotal.toFixed(2).replace('.', ',')}`;
     }
 }
+
+function verificaItemNaLista(lista, item) {
+    for (let i = 0; i < lista.length; i++) {
+      const [itemLista, quantidadeLista] = lista[i].split(',');
+      if (itemLista.trim() === item.trim()) {
+        return true;
+      }
+    }
+    return false;
+  }
 
 export { CaixaDaLanchonete };
 
